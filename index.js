6b43397a6aef13eca92a029b962f4247de2fd60d
santimendoza <smendoza@condorlabs.io>
2018-07-04 16:08:07 -0500

    Add middleware option to router group

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 
-express.Router.group = function(arg1, arg2) {
-  let handlerFunction, path;
-  if (arg2 === undefined) {
-    path = '/';
-    handlerFunction = arg1;
-  } else {
+express.Router.group = function(arg1, arg2, arg3) {
+  let handlerFunction = arg1;
+  let path = '/';
+  let middleware = undefined;
+
+  if (arg3) {
+    path = arg1;
+    middleware = arg2;
+    handlerFunction = arg3;
+  } else if (arg2) {
     path = arg1;
     handlerFunction = arg2;
   }
-
   const router = express.Router();
   handlerFunction(router);
+  if (middleware) {
+    this.use(middleware);
+  }
   this.use(path, router);
   return this;
 };
--- index.test.js
+++ index.test.js
@@ -26,4 +26,27 @@ describe(`Route Group`, () => {
       /^\/first-endpoint2\/?$/i.toString()
     );
   });
+
+  it('add middleware to router group', async () => {
+    function testMiddleware(req, res, next) {
+      console.log('Applying middleware');
+      next();
+    }
+    const group = router.group('/test', testMiddleware, router => {
+      router.get('/first-endpoint', () => {});
+      router.get('/first-endpoint2', () => {});
+    });
+    expect(router.stack.find(s => s.name === 'testMiddleware').name).equals(
+      'testMiddleware'
+    );
+    expect(router.stack[0].regexp.toString()).equals(
+      /^\/test\/?(?=\/|$)/i.toString()
+    );
+    expect(router.stack[0].handle.stack[0].regexp.toString()).contains(
+      /^\/first-endpoint\/?$/i.toString()
+    );
+    expect(router.stack[0].handle.stack[1].regexp.toString()).contains(
+      /^\/first-endpoint2\/?$/i.toString()
+    );
+  });
 });
